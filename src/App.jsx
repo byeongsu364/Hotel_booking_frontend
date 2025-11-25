f2615e333415390c96f677a8efe770a39139a864
janghyunjoon <135941433+janghyunjoon@users.noreply.github.com>
2025-11-25 19:10:01 +0900

    Hyunjoon2 (#7)
    * .
    
    * .
    
    * .
    
    ---------
    
    Co-authored-by: gkekals <gkekals12@kbu.ac.kr>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React from "react";
-import AppRouter from "./AppRouter"; // 라우트 정의 파일
+import { Routes, Route } from "react-router-dom";
 
-const App = () => {
-  return       <AppRouter />
-};
+import LoginPage from "./pages/auth/LoginPage";
+import SignupPage from "./pages/auth/SignupPage";
+import FindPasswordPage from "./pages/auth/FindPasswordPage";
+import EmailAuth from "./pages/auth/EmailAuth";
+import ResetPasswordPage from "./pages/auth/ResetPasswordPage";   // 🔥 추가
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/loginpage" element={<LoginPage />} />
+
+      <Route path="/signuppage" element={<SignupPage />} />
+
+      {/* 비밀번호 찾기 (이메일 입력 화면) */}
+      <Route path="/findpassword" element={<FindPasswordPage />} />
+
+      {/* 이메일 인증 */}
+      <Route path="/emailauth" element={<EmailAuth />} />
+
+      {/* 새 비밀번호 설정 */}
+      <Route path="/resetpassword" element={<ResetPasswordPage />} />  {/* 🔥 핵심 */}
+    </Routes>
+  );
+}
 
 export default App;
